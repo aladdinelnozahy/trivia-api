3a512a3be8a0f3c2f245d40986ad218e6a25d349
aladdinelnozahy <aladdin.elnozahy@gmail.com>
2021-01-19 11:21:05 +0200

    fixing errors

--- backend/models.py
+++ backend/models.py
@@ -1,18 +1,18 @@
-import os
-from sqlalchemy import Column, String, Integer, create_engine
+# import os
+# from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
-import json
+# import json
 
 database_name = "trivia"
 username = "postgres"
 password = "1234"
-host_port = "localhost:5432"
+port = "localhost:5432"
 # database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 database_path = "postgres://{}:{}@{}/{}".format(
     username,
     password,
-    host_port,
+    port,
     database_name)
 
 
@@ -22,6 +22,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -29,18 +31,21 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+
 '''
 Question
 
 '''
-class Question(db.Model):  
-  __tablename__ = 'questions'
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    category = Column(String)
+    difficulty = Column(Integer)
 
   def __init__(self, question, answer, category, difficulty):
     self.question = question
